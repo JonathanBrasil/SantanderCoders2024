ec64e49acca2619d48920fa82b3f739438493cf0
Jonathan Brasil <j_jonatha_2011@hotmail.com>
2024-07-17 19:15:33 -0300

    add aula 1

---
+++ Python/arquivo.py
@@ -0,0 +1,18 @@
+print('Hello World!')
+input('Qual linguagem vc esta estudando?')
+
+print('ok')
+
+'''
+ISSO AQUI SAO COMENTARIOS DE COMECO
+E
+FIM
+Comet
+'''
+# Comentario de linha
+
+# > Variável
+
+# 1. variáveis
+
+idade = 26 #variavel idade que recebe 26, nome e valor
